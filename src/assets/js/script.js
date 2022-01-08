231c8ff182cbb615cba6039999c3fba54f1c4687
lis98lis <89865689+lis98lis@users.noreply.github.com>
2022-01-09 00:21:57 +0300

    account.html full done

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,14 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 function filterBtn() {
-    document.querySelector(".catalog__filters").addEventListener("click", evt => {
-        document.querySelector(".filter-popup").classList.add("active")
-        document.querySelector(".background-blur").classList.add("active")
-    })
-    document.querySelector(".filter-popup__title-cross").addEventListener("click", evt => {
-        document.querySelector(".filter-popup").classList.remove("active")
-        document.querySelector(".background-blur").classList.remove("active")
-    })
+    if( document.querySelector(".catalog__filters")){
+        document.querySelector(".catalog__filters").addEventListener("click", evt => {
+            document.querySelector(".filter-popup").classList.add("active")
+            document.querySelector(".background-blur").classList.add("active")
+        })
+        document.querySelector(".filter-popup__title-cross").addEventListener("click", evt => {
+            document.querySelector(".filter-popup").classList.remove("active")
+            document.querySelector(".background-blur").classList.remove("active")
+        })
+    }
 }
 function handlerSwitch() {
     document.querySelectorAll(".switch").forEach(currentSwitch => {
